67af53e392b0e7757837e3206c67e62785e2a339
HAPPY YADAV <happy.hy0017@gmail.com>
2024-06-21 12:23:31 +0530

    counter app

---
+++ counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [value,setValue]= useState(0)
+  // let value =10;
+const addValue = ()=>{
+  if(value >= 20){
+    alert('Cannot increase value more than 20')
+  }
+  else{
+  value = value + 1;
+  setValue(value);
+  }
+  // console.log(`Value is ${value}`);
+}
+const decValue = ()=>{
+  if(value <=0){
+    alert('Cannot decrease value less than 0')
+  }
+  else{
+  value = value - 1;
+  setValue(value);
+  }
+  // console.log(`Value is ${value}`);
+}
+  return (
+    <>
+      <h1>Happy Yadav</h1>
+      <h2>Counter Value: {value}</h2>
+      <button
+      onClick={addValue}
+      >Increase Value</button>
+      <br />
+      <br />
+      <button
+      onClick={decValue}
+      >Decrease Value</button>
+      <p>{value}</p>
+    </>
+  )
+}
+
+export default App
