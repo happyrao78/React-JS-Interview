00806390b1c08983a98a4ec7e900291388d02d23
HAPPY YADAV <happy.hy0017@gmail.com>
2024-07-09 23:37:47 +0530

    backend+frontned project using appwrite

---
+++ miniblog/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <> 
+      <h1>A blog app using appwrite</h1>
+    </>
+  ) 
+}
+
+export default App
+ 
